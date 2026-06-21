0151a6baeec5318e2a84d29af38d6f15f145d231
jcdematos <github@jcdematos.com>
2026-06-21 09:08:49 -0300

    Create new module s3_client

--- bigquery_client.py
+++ bigquery_client.py
@@ -1,35 +1,12 @@
 import logging
 import hashlib
-import os
 from datetime import datetime, timezone
 from google.cloud import bigquery
 from google.api_core.exceptions import GoogleAPIError
+from s3_client import upload_to_s3
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-import boto3
-from botocore.exceptions import ClientError
-
-aws_access_key = os.environ['AWS_ACCESS_KEY_ID']
-aws_secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
-aws_region = 'us-east-1'
-
-
-def upload_to_s3(df, table, config) -> None:
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id=aws_access_key,
-        aws_secret_access_key=aws_secret_key,
-        region_name=aws_region
-    )
-
-    logger.info(f"[+] Saving {table} dataframe to file")
-    df.to_parquet(f'{table}.parquet', index=False)
-
-    parquet_file = f"{table}.parquet"
-    logger.info(f"Upload {parquet_file} to s3 bronze")
-    s3_client.upload_file(str(parquet_file), config['bucket']['name'], f'{config["paths"]["bronze"]}{parquet_file}')
 
 
 def ingest_from_bigquery(tables, config) -> None:
@@ -57,7 +34,7 @@ def ingest_from_bigquery(tables, config) -> None:
             errors='ignore'
         ).apply(lambda row: hashlib.md5(str(row.values).encode()).hexdigest(), axis=1)
 
-        logger.info("Registros inseridos: %s", len(df))
-        logger.info("   Colunes: %s", list(df.columns))
+        logger.info("Itens added: %s", len(df))
+        logger.info("   Columns: %s", list(df.columns))
 
         upload_to_s3(df, table, config)
