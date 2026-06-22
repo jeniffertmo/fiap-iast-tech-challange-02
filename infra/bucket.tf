f1b7cf63f5c56eee0e211d343a7d225c07ff66f0
jcdematos <github@jcdematos.com>
2026-06-21 21:26:33 -0300

    Add force destroy for testing

--- infra/bucket.tf
+++ infra/bucket.tf
@@ -5,6 +5,8 @@ resource "aws_s3_bucket" "medallion-bucket" {
     Name        = var.medallion-bucket
     Environment = "Dev"
   }
+
+  force_destroy = true
 }
 
 resource "aws_s3_object" "layers" {
@@ -12,13 +14,16 @@ resource "aws_s3_object" "layers" {
   bucket       = aws_s3_bucket.medallion-bucket.id
   key          = each.value
   content_type = "application/x-directory"
+
 }
 
 resource "aws_s3_bucket" "glue-scripts-bucket" {
-  bucket = "fiap-datalake-tech-glue-scripts"
+  bucket = local.glue_scripts_bucket
 
   tags = {
     Name        = "fiap-datalake-tech-glue-scripts"
     Environment = "Dev"
   }
+
+  force_destroy = true
 }
